8567fad64e94a7c1b9e0763a6226883c6e28bfd8
Marcus Vinícius <vmvini@hotmail.com>
2016-09-16 13:45:14 -0300

    dockerignore to ignore node_modules

--- node-client/app.js
+++ node-client/app.js
@@ -9,9 +9,6 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -47,11 +44,6 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
-
-
-
-
-
 
 
 // error handlers
